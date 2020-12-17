dda8fea0ab1ea446bf8576af5b077e531daed83c
Antony <a.yatsenko@aeronavigator.ru>
2020-12-17 14:29:28 +0300

    [ALL] Initial commit

---
+++ src/main/java/glim/antony/example/streaming/web/StreamingWebExampleApplication.java
@@ -0,0 +1,13 @@
+package glim.antony.example.streaming.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingWebExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingWebExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/glim/antony/example/streaming/web/StreamingWebExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package glim.antony.example.streaming.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingWebExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
